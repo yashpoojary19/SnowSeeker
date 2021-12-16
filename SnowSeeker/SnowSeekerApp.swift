3c9fb073594cd2852c748811aad557e547d0afc7
Yash Poojary <yashspoojary245@gmail.com>
2021-12-16 14:29:39 +0530

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Yash Poojary on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
